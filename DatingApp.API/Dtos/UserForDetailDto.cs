2b06cb405b4dbe01e0a36d8c4defd10137b6bc71
Paul <paul.guerra@gmail.com>
2019-09-13 11:28:46 -0600

    End of section 8

--- DatingApp.API/Dtos/UserForDetailDto.cs
+++ DatingApp.API/Dtos/UserForDetailDto.cs
@@ -1,18 +1,19 @@
 using System;
 using System.Collections.Generic;
+using DatingApp.API.Models;
 
-namespace DatingApp.API.Models
+namespace DatingApp.API.Dtos
 {
-    public class User
+    public class UserForDetailDto
     {
+        private IEnumerable<Photo> photos;
+
         public int Id { get; set; }
         public string Username { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt { get; set; }
 
         public string Gender { get; set; }
 
-        public DateTime DateOfBirth { get; set; }
+        public int Age { get; set; }
 
         public string KnownAs { get; set; }
 
@@ -29,8 +30,8 @@ namespace DatingApp.API.Models
 
         public string Country { get; set; }
 
-        public ICollection<Photo> Photos { get; set; }
+        public string PhotoUrl { get; set; }
 
-
+        public ICollection<PhotosForDetailDto> Photos { get; set; }
     }
 }
