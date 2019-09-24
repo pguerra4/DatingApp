93fd91b6de716114e45f95e7e5afb8cab8622c51
Paul <paul.guerra@gmail.com>
2019-09-24 11:47:22 -0600

    End of section 12

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -3,6 +3,15 @@ import { Component, OnInit, Output, EventEmitter, Input } from "@angular/core";
 import { AuthService } from "../_services/auth.service";
 import * as alertify from "alertifyjs";
 import { AlertifyService } from "../_services/alertify.service";
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from "@angular/forms";
+import { BsDatepickerConfig } from "ngx-bootstrap";
+import { User } from "../_models/user";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-register",
@@ -12,25 +21,71 @@ import { AlertifyService } from "../_services/alertify.service";
 export class RegisterComponent implements OnInit {
   @Input() valuesFromHome: any;
   @Output() cancelRegister = new EventEmitter();
+  registerForm: FormGroup;
+  bsConfig: Partial<BsDatepickerConfig>;
 
-  model: any = {};
+  user: User;
 
   constructor(
     private authService: AuthService,
-    private alertify: AlertifyService
+    private alertify: AlertifyService,
+    private router: Router,
+    private fb: FormBuilder
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    (this.bsConfig = {
+      containerClass: "theme-red"
+    }),
+      this.createregisterForm();
+  }
+
+  createregisterForm() {
+    this.registerForm = this.fb.group(
+      {
+        gender: ["male"],
+        username: ["", Validators.required],
+        knownAs: ["", Validators.required],
+        dateOfBirth: [null, Validators.required],
+        city: ["", Validators.required],
+        country: ["", Validators.required],
+        password: [
+          "",
+          [
+            Validators.required,
+            Validators.minLength(4),
+            Validators.maxLength(20)
+          ]
+        ],
+        confirmPassword: ["", Validators.required]
+      },
+      { validator: this.passwordMatchValidator }
+    );
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get("password").value === g.get("confirmPassword").value
+      ? null
+      : { mismatch: true };
+  }
 
   register() {
-    this.authService.register(this.model).subscribe(
-      () => {
-        alertify.success("registration successful");
-      },
-      error => {
-        alertify.error(error);
-      }
-    );
+    if (this.registerForm.valid) {
+      this.user = Object.assign({}, this.registerForm.value);
+      this.authService.register(this.user).subscribe(
+        () => {
+          alertify.success("registration successful");
+        },
+        error => {
+          alertify.error(error);
+        },
+        () => {
+          this.authService.login(this.user).subscribe(() => {
+            this.router.navigate(["/members"]);
+          });
+        }
+      );
+    }
   }
 
   cancel() {
